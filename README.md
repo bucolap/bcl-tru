1c08aae3c51bdd581daf2743ef40310e451a4d8c
BCL <buiconglap77@gmail.com>
2024-03-10 13:32:52 +0700

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+#Phep tru 2 so bat ky
+Phep trừ hai số bất kỳ, đơn giản, dễ sử dụng
---
+++ tru.js
@@ -0,0 +1,4 @@
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+const sub = (a, b) => a - b
+exports.sub = sub
